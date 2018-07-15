616e79f0c81b016bc94b83a4507418e0bb2876ea
Cédric Nanteau <cedric.nanteau@gmail.com>
2018-07-15 12:12:41 +0200

    tag name is now unique

--- models/Tag.js
+++ models/Tag.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const TagSchema = Schema({
     name: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     tagAssociated: [{
         type: Schema.Types.ObjectId,
