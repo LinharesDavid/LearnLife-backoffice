b93266b9d5bfb49b8f7ba207b2e8ca40fc1b2366
Cédric Nanteau <cedric.nanteau@gmail.com>
2018-07-15 12:18:59 +0200

    category name is now unique

--- models/Category.js
+++ models/Category.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const CategorySchema = Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     }
 });
 
