cf96dd38d689053deeeb24067a31885fb3cd74ac
Linhares David <linhares.david@digipolitan.com>
2018-07-23 14:59:25 +0200

    Fixed dumb issue on challenges user list

--- controllers/userchallenges/user_list.js
+++ controllers/userchallenges/user_list.js
@@ -2,7 +2,7 @@ module.exports = server => {
     const UserChallenge = server.models.UserChallenge;
 
     return (req, res, next) => {
-        UserChallenge.find({userId: req.params.id})
+        UserChallenge.find({user: req.params.userId})
             .populate({path: 'challenge'})
             .then(uc => res.send(uc))
             .catch(err => err.message || err)
