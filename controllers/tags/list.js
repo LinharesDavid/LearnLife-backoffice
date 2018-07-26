08f3ba035373d60ab2f8ec9318b14a5725c541dc
Linhares David <linhares.david@digipolitan.com>
2018-07-26 11:46:30 +0200

    quick fix

--- controllers/tags/list.js
+++ controllers/tags/list.js
@@ -3,6 +3,7 @@ module.exports = server => {
 
     return (req, res, next) => {
         Tag.find()
+            .populate({path: "tagAssociated"})
             .then(tag=> res.send(tag))
             .catch(error => res.status(500).send(error.message || error))
     }
